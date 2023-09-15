d1458731916caf252da0f4c282f66beef77c5f0b
darkseiD779966 <ibrahim.darkseid@gmail.com>
2023-09-15 14:52:42 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./home.css";
+function Home() {
+  return (
+    <section id="home">
+        <div className='cont'>
+            <div className='container'>
+               <h3>
+                sdasdasdasdasdasdasd</h3> <p>asdasdasdasdasdasdasdasd</p>
+                <a>dssadads</a>
+            </div>
+        </div>
+        </section>
+  )
+}
+
+export default Home
